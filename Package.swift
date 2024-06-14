7694120032b9b70b55b947b03dec1b945ef6e529
Vytautas Vasiliauskas <vytautas.vasiliauskas@teliacompany.com>
2024-06-14 14:27:22 +0300

    Fix dependencies

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "tuist-lint",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v12)],
     products: [
         .executable(
             name: "tuist-lint",
@@ -13,9 +13,9 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/tuist/ProjectAutomation", .exact("3.2.0")),
-        .package(url: "https://github.com/realm/SwiftLint", revision: "e497f1f"), // v0.47.1, commit SHA due to its use of unsafe flags
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.0.0")),
+        .package(url: "https://github.com/tuist/ProjectAutomation", .exact("4.9.0")),
+        .package(url: "https://github.com/realm/SwiftLint", revision: "eb85125"), // v0.51.0, commit SHA due to its use of unsafe flags
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.2.1")),
     ],
     targets: [
         .executableTarget(
